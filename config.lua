9f1aa341e1e337cc70da147254e69f5a24000517
Junbo <junblu@qti.qualcomm.com>
2022-11-21 13:33:39 +0800

    Add more

--- config.lua
+++ config.lua
@@ -6,36 +6,32 @@
 -- Enable powershell as your default shell
 if (vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1)
 then
-    vim.opt.shell = "pwsh.exe -NoLogo"
-    vim.opt.shellcmdflag =
-    "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
-    vim.cmd [[
-    let &shellredir = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
-    let &shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
-    set shellquote= shellxquote=
-    ]]
-    vim.opt.guifont = "Courier New:h14" -- the font used in graphical neovim applications
-    -- Set a compatible clipboard manager
-    vim.g.clipboard = {
-        copy = {
-            ["+"] = "win32yank.exe -i --crlf",
-            ["*"] = "win32yank.exe -i --crlf",
-        },
-        paste = {
-            ["+"] = "win32yank.exe -o --lf",
-            ["*"] = "win32yank.exe -o --lf",
-        },
-    }
+	vim.opt.shell = "pwsh.exe -NoLogo"
+	vim.opt.shellcmdflag =
+	"-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
+	vim.cmd [[
+		let &shellredir = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+		let &shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+		set shellquote= shellxquote=
+		]]
+	vim.opt.guifont = "Courier New:h14" -- the font used in graphical neovim applications
+	-- Set a compatible clipboard manager
+	vim.g.clipboard = {
+		copy = {
+			["+"] = "win32yank.exe -i --crlf",
+			["*"] = "win32yank.exe -i --crlf",
+		},
+		paste = {
+			["+"] = "win32yank.exe -o --lf",
+			["*"] = "win32yank.exe -o --lf",
+		},
+	}
 end
 
 vim.opt.timeoutlen = 100 -- time to wait for a mapped sequence to complete (in milliseconds)
 vim.opt.clipboard = "unnamedplus" -- allows neovim to access the system clipboard
 vim.opt.cmdheight = 2 -- more space in the neovim command line for displaying messages
-vim.opt.completeopt = { "menuone", "noselect" }
 vim.opt.conceallevel = 0 -- so that `` is visible in markdown files
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
 vim.opt.rnu = true
 vim.opt.cursorcolumn = true
 
@@ -80,12 +76,12 @@ lvim.keys.insert_mode["<C-k>"] = "<C-w>k"
 
 if (vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1)
 then
-    lvim.keys.normal_mode["<C-v>"] = "<Esc>\"+gP"
-    lvim.keys.insert_mode["<C-v>"] = "<Esc>\"+gP"
+	lvim.keys.normal_mode["<C-v>"] = "<Esc>\"+gP"
+	lvim.keys.insert_mode["<C-v>"] = "<Esc>\"+gP"
 elseif (vim.fn.has("macunix") == 1)
 then
-    lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
-    lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
+	lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
+	lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
 end
 -- unmap a default keymapping
 vim.keymap.del("n", "<C-Up>")
@@ -97,40 +93,41 @@ vim.keymap.del("n", "<C-Up>")
 -- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
 local _, actions = pcall(require, "telescope.actions")
 lvim.builtin.telescope.defaults.mappings = {
-    -- for input mode
-    i = {
-        ["<C-j>"] = actions.move_selection_next,
-        ["<C-k>"] = actions.move_selection_previous,
-        ["<C-n>"] = actions.cycle_history_next,
-        ["<C-p>"] = actions.cycle_history_prev,
-    },
-    -- for normal mode
-    n = {
-        ["<C-j>"] = actions.move_selection_next,
-        ["<C-k>"] = actions.move_selection_previous,
-    },
+	-- for input mode
+	i = {
+		["<C-j>"] = actions.move_selection_next,
+		["<C-k>"] = actions.move_selection_previous,
+		["<C-n>"] = actions.cycle_history_next,
+		["<C-p>"] = actions.cycle_history_prev,
+	},
+	-- for normal mode
+	n = {
+		["<C-j>"] = actions.move_selection_next,
+		["<C-k>"] = actions.move_selection_previous,
+	},
 }
 
 -- Use which-key to add extra bindings with the leader-key prefix
-lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
-lvim.builtin.which_key.mappings["t"] = {
-  name = "+Trouble",
-  r = { "<cmd>Trouble lsp_references<cr>", "References" },
-  f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
-  d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
-  q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
-  l = { "<cmd>Trouble loclist<cr>", "LocationList" },
-  w = { "<cmd>Trouble workspace_diagnostics<cr>", "Workspace Diagnostics" },
-}
 lvim.builtin.which_key.mappings["c"] = {}
 lvim.builtin.which_key.mappings["f"] = {
-    f = { "<cmd>Telescope find_files<cr>", "Find Files"},
-    h = { "<cmd>Telescope help_tags<cr>", "Vim Tags" },
-    r = { "<cmd>Telescope oldfiles<cr>", "History files" },
+  name = "+Find",
+	f = { "<cmd>Telescope find_files<cr>", "Find Files"},
+	h = { "<cmd>Telescope help_tags<cr>", "Vim Tags" },
+	r = { "<cmd>Telescope oldfiles<cr>", "History files" },
 }
 lvim.builtin.which_key.mappings["h"] = {}
 lvim.builtin.which_key.mappings["bd"] = {
-    "<cmd>BufferKill<cr>", "Kill Buffer"
+	"<cmd>BufferKill<cr>", "Kill Buffer"
+}
+lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
+lvim.builtin.which_key.mappings["t"] = {
+	name = "+Trouble",
+	r = { "<cmd>Trouble lsp_references<cr>", "References" },
+	f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
+	d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
+	q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
+	l = { "<cmd>Trouble loclist<cr>", "LocationList" },
+	w = { "<cmd>Trouble workspace_diagnostics<cr>", "Workspace Diagnostics" },
 }
 
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
@@ -139,40 +136,40 @@ lvim.builtin.alpha.mode = "dashboard"
 
 if (vim.fn.has('win32') == 1 or vim.fn.has('win64') == 1)
 then
-  lvim.builtin.terminal.active = false
-  lvim.builtin.terminal.shell = "pwsh.exe -NoLogo"
+	lvim.builtin.terminal.active = false
+	lvim.builtin.terminal.shell = "pwsh.exe -NoLogo"
 
-  -- nvim-tree has some performance issues on windows, see kyazdani42/nvim-tree.lua#549
-  lvim.builtin.nvimtree.setup.diagnostics.enable = nil
-  lvim.builtin.nvimtree.setup.filters.custom = nil
-  lvim.builtin.nvimtree.setup.git.enable = nil
-  lvim.builtin.nvimtree.setup.update_cwd = nil
-  lvim.builtin.nvimtree.setup.update_focused_file.update_cwd = nil
-  lvim.builtin.nvimtree.setup.view.side = "left"
-  lvim.builtin.nvimtree.setup.renderer.highlight_git = nil
-  lvim.builtin.nvimtree.setup.renderer.icons.show.git = nil
+	-- nvim-tree has some performance issues on windows, see kyazdani42/nvim-tree.lua#549
+	lvim.builtin.nvimtree.setup.diagnostics.enable = nil
+	lvim.builtin.nvimtree.setup.filters.custom = nil
+	lvim.builtin.nvimtree.setup.git.enable = nil
+	lvim.builtin.nvimtree.setup.update_cwd = nil
+	lvim.builtin.nvimtree.setup.update_focused_file.update_cwd = nil
+	lvim.builtin.nvimtree.setup.view.side = "left"
+	lvim.builtin.nvimtree.setup.renderer.highlight_git = nil
+	lvim.builtin.nvimtree.setup.renderer.icons.show.git = nil
 else
-  lvim.builtin.terminal.active = true
-  lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
+	lvim.builtin.terminal.active = true
+	lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
 end
 
 
 -- if you don't want all the parsers change this to a table of the ones you want
 if (vim.fn.has('win32') == 1 or vim.fn.has('win64') == 1)
 then
-  lvim.builtin.treesitter.ensure_installed = {
-    "c",
-    "lua",
-  }
+	lvim.builtin.treesitter.ensure_installed = {
+		"c",
+		"lua",
+	}
 else
-  lvim.builtin.treesitter.ensure_installed = {
-    "bash",
-    "c",
-    "json",
-    "lua",
-    "python",
-    "yaml",
-  }
+	lvim.builtin.treesitter.ensure_installed = {
+		"bash",
+		"c",
+		"json",
+		"lua",
+		"python",
+		"yaml",
+	}
 end
 
 lvim.builtin.treesitter.ignore_install = { "haskell" }
@@ -256,77 +253,100 @@ lvim.builtin.treesitter.highlight.enable = true
 
 -- Additional Plugins
 lvim.plugins = {
-    {
-        "folke/trouble.nvim",
-        cmd = "TroubleToggle",
-    },
-    {
-        "p00f/nvim-ts-rainbow",
-    },
-    {
-        "karb94/neoscroll.nvim",
-        event = "WinScrolled",
-        config = function()
-            require('neoscroll').setup({
-                -- All these keys will be mapped to their corresponding default scrolling animation
-                mappings = {'<C-u>', '<C-d>', '<C-b>', '<C-f>',
-                    '<C-y>', '<C-e>', 'zt', 'zz', 'zb'},
-                hide_cursor = true,          -- Hide cursor while scrolling
-                stop_eof = true,             -- Stop at <EOF> when scrolling downwards
-                use_local_scrolloff = false, -- Use the local scope of scrolloff instead of the global scope
-                respect_scrolloff = false,   -- Stop scrolling when the cursor reaches the scrolloff margin of the file
-                cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
-                easing_function = nil,        -- Default easing function
-                pre_hook = nil,              -- Function to run before the scrolling animation starts
-                post_hook = nil,              -- Function to run after the scrolling animation ends
-            })
-        end
-    },
-    {
-        "kevinhwang91/rnvimr",
-        cmd = "RnvimrToggle",
-        config = function()
-            vim.g.rnvimr_draw_border = 1
-            vim.g.rnvimr_pick_enable = 1
-            vim.g.rnvimr_bw_enable = 1
-        end,
-    },
-    {
-        "folke/todo-comments.nvim",
-        event = "BufRead",
-        config = function()
-            require("todo-comments").setup()
-        end,
-    },
-    {
-        "itchyny/vim-cursorword",
-        event = {"BufEnter", "BufNewFile"},
-        config = function()
-            vim.api.nvim_command("augroup user_plugin_cursorword")
-            vim.api.nvim_command("autocmd!")
-            vim.api.nvim_command("autocmd FileType NvimTree,lspsagafinder,dashboard,vista let b:cursorword = 0")
-            vim.api.nvim_command("autocmd WinEnter * if &diff || &pvw | let b:cursorword = 0 | endif")
-            vim.api.nvim_command("autocmd InsertEnter * let b:cursorword = 0")
-            vim.api.nvim_command("autocmd InsertLeave * let b:cursorword = 1")
-            vim.api.nvim_command("augroup END")
-        end
-    },
-    {
-        "sindrets/diffview.nvim",
-        event = "BufRead",
-    },
+	{
+		"folke/trouble.nvim",
+		cmd = "TroubleToggle",
+	},
+	{
+		"p00f/nvim-ts-rainbow",
+	},
+	{
+		"karb94/neoscroll.nvim",
+		event = "WinScrolled",
+		config = function()
+			require('neoscroll').setup({
+				-- All these keys will be mapped to their corresponding default scrolling animation
+				mappings = {'<C-u>', '<C-d>', '<C-b>', '<C-f>',
+					'<C-y>', '<C-e>', 'zt', 'zz', 'zb'},
+				hide_cursor = true,          -- Hide cursor while scrolling
+				stop_eof = true,             -- Stop at <EOF> when scrolling downwards
+				use_local_scrolloff = false, -- Use the local scope of scrolloff instead of the global scope
+				respect_scrolloff = false,   -- Stop scrolling when the cursor reaches the scrolloff margin of the file
+				cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
+				easing_function = nil,        -- Default easing function
+				pre_hook = nil,              -- Function to run before the scrolling animation starts
+				post_hook = nil,              -- Function to run after the scrolling animation ends
+			})
+		end
+	},
+	{
+		"kevinhwang91/rnvimr",
+		cmd = "RnvimrToggle",
+		config = function()
+			vim.g.rnvimr_draw_border = 1
+			vim.g.rnvimr_pick_enable = 1
+			vim.g.rnvimr_bw_enable = 1
+		end,
+	},
+	{
+		"folke/todo-comments.nvim",
+		event = "BufRead",
+		config = function()
+			require("todo-comments").setup()
+		end,
+	},
+	{
+		"itchyny/vim-cursorword",
+		event = {"BufEnter", "BufNewFile"},
+		config = function()
+			vim.api.nvim_command("augroup user_plugin_cursorword")
+			vim.api.nvim_command("autocmd!")
+			vim.api.nvim_command("autocmd FileType NvimTree,lspsagafinder,dashboard,vista let b:cursorword = 0")
+			vim.api.nvim_command("autocmd WinEnter * if &diff || &pvw | let b:cursorword = 0 | endif")
+			vim.api.nvim_command("autocmd InsertEnter * let b:cursorword = 0")
+			vim.api.nvim_command("autocmd InsertLeave * let b:cursorword = 1")
+			vim.api.nvim_command("augroup END")
+		end
+	},
+	{
+		"sindrets/diffview.nvim",
+		event = "BufRead",
+	},
 }
+
+vim.api.nvim_create_augroup("indentGroup")
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
 vim.api.nvim_create_autocmd("BufEnter", {
-  pattern = { "*.json", "*.jsonc" },
-  -- enable wrap mode for json files only
-  command = "setlocal wrap",
+	pattern = { "*.json", "*.jsonc" },
+	group   = "indentGroup",
+	-- enable wrap mode for json files only
+	command = "setlocal wrap",
 })
+
+vim.api.nvim_create_autocmd("BufEnter", {
+	pattern = { "*.lua" },
+	group   = "indentGroup",
+	command = "setlocal nowrap tabstop=2 shiftwidth=2 noexpandtab",
+})
+
+vim.api.nvim_create_autocmd("BufEnter", {
+	pattern = { "*.c", "*.cpp", "*.h", "*.py", "*.php" },
+	group   = "indentGroup",
+	command = "setlocal nowrap tabstop=4 shiftwidth=4 softtabstop=4 expandtab textwidth=120"
+})
+
+vim.api.nvim_create_autocmd("BufEnter", {
+	pattern = { "*.txt", "*.log", "*.mk", "*.cmake", "Makefile", "makefile" },
+	group   = "indentGroup",
+	command = "setlocal nowrap tabstop=4 shiftwidth=4 noexpandtab"
+})
+
+
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = "zsh",
-  callback = function()
-    -- let treesitter use bash highlight for zsh files as well
-    require("nvim-treesitter.highlight").attach(0, "bash")
-  end,
+	pattern = "zsh",
+	callback = function()
+		-- let treesitter use bash highlight for zsh files as well
+		require("nvim-treesitter.highlight").attach(0, "bash")
+	end,
 })
