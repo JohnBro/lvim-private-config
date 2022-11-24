b9d2f3c820fa4ab0d2fb9c3eac1746f2bda2f35b
Junbo <johnbro@foxmail.com>
2022-11-24 14:02:10 +0800

    Fix more

--- lua/user/lsp.lua
+++ lua/user/lsp.lua
@@ -5,11 +5,11 @@ M.config = function()
 	lvim.lsp.installer.setup.ensure_installed = {
 		"sumneko_lua",
 		"jsonls",
+		"pylsp",
 	}
 
 	--- WARN: configure a server manually. !!Requires `:LvimCacheReset` to take effect!!
 	--- see the full default list `:lua print(vim.inspect(lvim.lsp.automatic_configuration.skipped_servers))`
-	vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "clangd" })
 	vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "pyright" })
 
 	lvim.lsp.installer.setup.automatic_installation = false
